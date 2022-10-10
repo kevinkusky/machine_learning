f455ab3ee07c3ebfad3d195392c83f9daa39b1c6
Kevin Kusky <kevinkusky@gmail.com>
2022-10-10 13:06:47 -0400

    cost function

---
+++ supervised_learning/cost_function.py
@@ -0,0 +1,30 @@
+import numpy as np
+# %matplotlib widget
+# import matplotlib.pyplot as plt
+# from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+# plt.style.use('./deeplearning.mplstyle')
+
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum += cost  
+    total_cost = (1 / (2 * m)) * cost_sum  
+
+    return total_cost
